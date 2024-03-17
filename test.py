4517e20aefaa304626ccafcd38932b9a7d1e43ba
Rafał Zaborowski <raffik2000@gmail.com>
2024-03-17 22:50:45 +0100

    add algorythm

--- test.py
+++ test.py
@@ -15,17 +15,39 @@ knn_model = NearestNeighbors(n_neighbors=2, metric='euclidean')
 knn_model.fit(X)
 
 # Przykładowa osoba dla której chcemy uzyskać rekomendacje
-sample_person = X.iloc[0].values.reshape(1, -1)
+person_index = 0
+sample_person = X.iloc[person_index].values.reshape(1, -1)
 
 # Znalezienie najbliższych sąsiadów
-distances, indices = knn_model.kneighbors(sample_person)
+neighbors_count = 4
+distances, indices = knn_model.kneighbors(sample_person, neighbors_count + 1)
 
-print("Indeksy najbliższych sąsiadów:")
-print(indices)
+new_data = data.iloc[indices[0]].copy()
 
-print("Rekomendacje dla danej osoby:")
-print(data.iloc[indices[0]])
+column_count = new_data.shape[1]
+for column_index in range(column_count):
+    if column_index == 0:
+        continue
 
-data.iloc[indices[0]].to_csv('wyniki_rekomendacji.csv', index=False)
+    column = new_data.iloc[:, column_index]
+    if pd.to_numeric(column.iloc[0], errors='coerce') >= 4:
+        new_data.iloc[0, column_index] = 'TAK'
+    elif pd.to_numeric(column.iloc[0], errors='coerce') < 4:
+        new_data.iloc[0, column_index] = 'NIE'
+    else:
+        number_of_people_rating = 0
+        rating_sum = 0
+        for neighbor_index in range(neighbors_count):
+            rating = pd.to_numeric(column.iloc[neighbor_index + 1], errors='coerce')
+            if not pd.isnull(rating):
+                number_of_people_rating += 1
+                rating_sum += rating
 
+        if number_of_people_rating == 0:
+            new_data.iloc[0, column_index] = 'BRAK_DANYCH'
+        elif rating_sum / number_of_people_rating >= 4:
+            new_data.iloc[0, column_index] = 'TAK'
+        else:
+            new_data.iloc[0, column_index] = 'NIE'
 
+new_data.to_csv('wyniki_rekomendacji.csv', sep=',', index=False)
